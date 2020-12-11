f01738c4fa26247117b6250f91b67bda067eff67
tripincloud <redataleb568@gmail.com>
2020-12-12 00:47:53 +0100

    Add files via upload

--- Fonctions.py
+++ Fonctions.py
@@ -21,6 +21,16 @@ def CleanHorizontal(liste):
                 i.remove(j)
     return liste
 
+"""def OffsetChecker(liste):
+    i = 0
+    while (i < len(liste)):
+        if ((len(liste[i])-1) < int(liste[i+1][0],0)):
+            print("la ligne "+str(i)+" est incomplète")
+        elif ((len(liste[i])-1) > int(liste[i+1][0],16)):
+            liste.remove(liste[i])
+        i = i + 1"""
+
+
 def NBtrames(liste):
     cpt = 0
     for i in liste :
@@ -29,48 +39,102 @@ def NBtrames(liste):
     return cpt
 
 
-def Extract(liste,Ethernet,IP):
-    curseurl=0
-    curseurc=0
+def Extract(liste,Ethernet,IP,TCP):
+    trame=0
     temp=""
-    cptE=14
     cptIP=0
+    
     for i in range(len(liste)):
         if (int(liste[i][0],0) == 0) :
-                for j in range(i,len(liste)):
-                        for k in range(1,len(liste[j])):
-                            if(cptE > 0):
-                                Ethernet.append(liste[j][k])
-                                cptE = cptE -1
-                                curseurl = j
-                                curseurc = k
+            Ethernet.append([])
+            IP.append([])
+            TCP.append([])
+            cptE=14
+            cptTCP=13
+            curseurl=0
+            curseurc=0
 
-                if (len(liste[curseurl])-(k+1) == 0):
-                    curseurl = curseurl+1
-                    curseurc = curseurc+1
-                    temp = liste[curseurl][curseurc]
-                    cptIP = int(temp[1],16)*4
-                else :
-                    curseurl = curseurl
-                    curseurc = curseurc+1
-                    temp = liste[curseurl][curseurc]
-                    cptIP = int(temp[1],16)*4
+            #Ethernet
+            for j in range(i,len(liste)):
+                for k in range(1,len(liste[j])):
+                    if(cptE > 0):
+                        Ethernet[trame].append(liste[j][k])
+                        cptE = cptE -1
+                        curseurl = j
+                        curseurc = k
 
-                for l in range (curseurl,len(liste)):
-                    print(l)
-                    for m in range (curseurc,len(liste[l])):
-                        print(m)
-                        if(m == len(liste[l])-1 and cptIP > 0):
-                            IP.append(liste[l][m])
-                            cptIP = cptIP -1
-                            curseurc = 1
-                            m = 0
-                        elif(cptIP > 0):
-                            IP.append(liste[l][m])
-                            cptIP = cptIP -1
-                            curseurl = l
-                            curseurc = m     
+            #IP
+            if (len(liste[curseurl])-(curseurc+1) == 0):
+                curseurl = curseurl+1
+                curseurc = curseurc+1
+                temp = liste[curseurl][curseurc]
+                cptIP = int(temp[1],16)*4
+            else :
+                curseurl = curseurl
+                curseurc = curseurc+1
+                temp = liste[curseurl][curseurc]
+                cptIP = int(temp[1],16)*4
 
+            for l in range (curseurl,len(liste)):
+                for m in range (curseurc,len(liste[l])):
+                    if(m == len(liste[l])-1 and cptIP > 0):
+                        IP[trame].append(liste[l][m])
+                        cptIP = cptIP -1
+                        curseurl = l 
+                        curseurc = 1
+                    elif(cptIP > 0):
+                        IP[trame].append(liste[l][m])
+                        cptIP = cptIP -1
+                        curseurl = l
+                        curseurc = m
+
+            #TCP
+            if (len(liste[curseurl])-(curseurc+1) == 0):
+                curseurl = curseurl+1
+                curseurc = curseurc+1
+            else :
+                curseurl = curseurl
+                curseurc = curseurc+1
+
+            for o in range (curseurl,len(liste)):
+                for p in range (curseurc,len(liste[o])):
+                    if(p == len(liste[l])-1 and cptTCP > 0):
+                        TCP[trame].append(liste[o][p])
+                        cptTCP = cptTCP -1
+                        curseurl = o
+                        curseurc = 1
+                    elif(cptTCP > 0):
+                        TCP[trame].append(liste[o][p])
+                        cptTCP = cptTCP -1
+                        curseurl = o
+                        curseurc = p
+
+            if (len(liste[curseurl])-(curseurc+1) == 0):
+                temp = liste[curseurl][curseurc]
+                cptTCP = (int(temp[0],16)*4)-13
+                curseurl = curseurl+1
+                curseurc = curseurc+1
+            else :
+                temp = liste[curseurl][curseurc]
+                cptTCP = (int(temp[0],16)*4)-13
+                curseurl = curseurl
+                curseurc = curseurc+1
+            
+            for o in range (curseurl,len(liste)):
+                for p in range (curseurc,len(liste[o])):
+                    if(p == len(liste[l])-1 and cptTCP > 0):
+                        TCP[trame].append(liste[o][p])
+                        cptTCP = cptTCP -1
+                        curseurl = o
+                        curseurc = 1
+                    elif(cptTCP > 0):
+                        TCP[trame].append(liste[o][p])
+                        cptTCP = cptTCP -1
+                        curseurl = o
+                        curseurc = p
+
+            trame = trame +1
+                        
 
             
 
